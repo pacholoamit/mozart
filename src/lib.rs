23248b2b24c28c115282fa4bef9187c38bed7e16
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-22 09:48:12 +0000

    Add get method to Cache

--- src/lib.rs
+++ src/lib.rs
@@ -27,13 +27,22 @@ impl Cache {
         }
     }
 
-    pub fn set(&mut self, key: String, value: String) -> Result<bool, &'static str> {
-        let result = match self.store.insert(key, value) {
-            Some(_) => Ok(true),
-            None => Err("Key does not exit in cache"),
+    pub fn set(&mut self, key: &str, value: &str) -> Result<(), &'static str> {
+        let is_success = match self.store.insert(key.into(), value.into()) {
+            Some(_) => Err("Key already exists in cache"),
+            None => Ok(()),
         };
 
-        return result;
+        return is_success;
+    }
+
+    pub fn get(&mut self, key: &str) -> Result<&String, &'static str> {
+        let value = match self.store.get(key) {
+            Some(v) => Ok(v),
+            None => Err("Key does not exist in cache"),
+        };
+
+        return value;
     }
 }
 
@@ -61,16 +70,41 @@ mod tests {
     fn test_set() {
         let mut cache = Cache::default();
 
+        let key = "foo";
+        let value = "123";
+
         // Add a key-value pair to the cache
-        let result = cache.set("foo".to_string(), "123".to_string());
+        let result = cache.set(&key, &value);
 
         // Check that the method returns Ok
-        assert_eq!(result, Ok(true));
+        assert_eq!(result, Ok(()));
+
+        let second_value = "456";
 
         // Add another key-value pair with the same key
-        let result = cache.set("foo".to_string(), "456".to_string());
+        let result = cache.set(&key, &second_value);
 
         // Check that the method returns an Err with the correct error message
         assert_eq!(result, Err("Key already exists in cache"));
+    }
+
+    #[test]
+    fn test_get() {
+        let mut cache = Cache::new(60, false);
+
+        // Add a key-value pair to the cache
+        cache.set("foo", "123").unwrap();
+
+        // Get the value associated with the key
+        let result = cache.get("foo");
+
+        // Check that the method returns Ok with the correct value
+        assert_eq!(result, Ok(&"123".to_string()));
+
+        // Try to get the value associated with a non-existent key
+        let result = cache.get("bar");
+
+        // Check that the method returns an Err with the correct error message
+        assert_eq!(result, Err("Key does not exist in cache"));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use mozart::Cache;
 
 fn main() {
-    let cache = Cache::new(0, false);
+    let mut cache = Cache::new(0, false);
+
+    cache.set("Example", "123").unwrap();
+
+    let result = cache.get("Example").unwrap();
+
+    println!("{}", result)
 }
