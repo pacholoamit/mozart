c178e6fd5ddd5cdcf19c7dce2dcec37c682ff00f
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-24 07:35:18 +0000

    Add Makefile

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,16 @@
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
-#[allow(dead_code)]
 pub enum Error {
     #[error("Generic {0}")]
     Generic(String),
 
+    #[error("Key `{0}` does not exist in cache")]
+    CacheKeyNotFound(String),
+}
+
+#[derive(Error, Debug, PartialEq)]
+pub enum CacheError {
     #[error("Key `{0}` does not exist in cache")]
     CacheKeyNotFound(String),
 }
