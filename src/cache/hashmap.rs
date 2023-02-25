dfecc35896b5cb43e207375d92480d0869a8874e
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-25 19:58:49 +0000

    Refactor tests in hashmap.rs to use module tests

--- src/cache/hashmap.rs
+++ src/cache/hashmap.rs
@@ -56,10 +56,10 @@ impl Cache for HashMapCache {
 }
 
 #[cfg(test)]
-mod hashmap_cache_tests {
-    use serde_json::json;
+mod tests {
 
     use super::*;
+    use serde_json::json;
 
     #[test]
     fn test_default_cache() {
