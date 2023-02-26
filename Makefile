2ea83dbde625a62fd2d7d68033bc55cc1552bc39
pacholoamit <pacholoamit.tech@gmail.com>
2023-02-26 13:44:46 +0000

    Scaffold cache proto & gRPC

--- Makefile
+++ Makefile
@@ -1,20 +1,30 @@
-.PHONY: check-cargo-watch run
+.PHONY: install-cargo-watch check-protoc install run-server run-client doc dev
 
 
-check-cargo-watch:
+install-cargo-watch:
 	@which cargo-watch > /dev/null || cargo install cargo-watch
 
+check-protoc:
+	@which protoc > /dev/null || echo "Please install protoc"
+
+install:
+	@cargo install --path .
+	@make install-cargo-watch
+	@make check-protoc
+
 run-server:
-	@cargo watch -q -c -x "run --bin mozart-server"
+	@cargo watch -q -c -x "test" 
+	@cargo watch -q -c -x "run --bin server"
 
 run-client:
-	@cargo watch -q -c -x "run --bin mozart-client"
+	@cargo watch -q -c -x "test" 
+	@cargo watch -q -c -x "run --bin client"
 
 doc:
 	@cargo doc
 	@cargo docserver -p 4545
-	
+
+
 dev:
-	@make check-cargo-watch
 	@cargo watch -q -c -x "test" 
 	
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,39 +1,12 @@
 #![allow(unused)]
-
 use anyhow::Result;
-use mozart::cache::*;
-use serde_json::json;
-use tokio::{
-    io::{self, AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
-};
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod cache {
+    tonic::include_proto!("cache");
+}
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
-    let listener = TcpListener::bind(&format!("127.0.0.1:{}", 8080)).await?;
-
-    println!("Started server on port 8080");
-
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-        socket.readable().await?;
-
-        let mut buf = [0; 4096];
-        match socket.read(&mut buf).await {
-            Ok(n) if n == 0 => break,
-            Ok(n) => {
-                let data = std::str::from_utf8(&buf[..n])?;
-                println!("read {} bytes: {}", n, data);
-                socket.write_all(data.as_bytes()).await?;
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                continue;
-            }
-            Err(e) => {
-                return Err(e.into());
-            }
-        }
-    }
-
     Ok(())
 }
